9dac104077dd5a2620a7a2f374393ec35ff87499
[Kevin] [Ruat] <[rukerdo99bg]@[gmail.com]>
2020-11-22 12:07:31 +0100

    Avancement jeu

--- jeu.h
+++ jeu.h
@@ -1,8 +1,16 @@
 #ifndef JEU_H
 #define JEU_H
+#include "terrain.h"
 
 class jeu{
+public:
+    jeu();
+    ~jeu();
+    void iniTerrain();
+    void lancerPartie(); //boucle de jeu
 
+private :
+    terrain terrainDeJeu;
 
 };
 
