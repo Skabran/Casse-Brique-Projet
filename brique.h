f7ccbfc1d8a548ab22edb65ba562948981b669a4
[Kevin] [Ruat] <[rukerdo99bg]@[gmail.com]>
2020-12-30 21:34:25 +0100

    Docu doxygen de brique.h

--- brique.h
+++ brique.h
@@ -2,14 +2,29 @@
 #define BRIQUE_H
 #include "element.h"
 
+/**
+Classe de la brique de base du jeu. Herite de la classe element
+*/
 class brique : public element{
 public:
+///Constructeur par default de la brique. Element par default d_longueur = 4.0 et d_largeur = 2.0
     brique();
+///Constructeur de la brique. Element(x, y), d_longueur = longueur et d_largeur = largeur
     brique(double x, double y, double longueur, double largeur);
+///Destructeur de la brique par default
     ~brique();
+
+/**
+A FAIRE
+*/
     bool collision() override;
+
+
 private:
+    /** longueur de la brique (taille selon l'axe x ) */
     double d_longueur;
+
+    /** largeur de la brique (taille selon l'axe y ) */
     double d_largeur;
 };
 
